a00c83bd07700169ce93d730f42260de847db26e
Weldin Dunn <weldindunn20@gmail.com>
2022-07-08 16:38:43 -0400

    Added upgrade icons for linemen, gas, and solar

--- src/components/upgradeSquare.tsx
+++ src/components/upgradeSquare.tsx
@@ -3,7 +3,10 @@ import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Upgrade } from "../interfaces/upgrade";
 import { UpgradeHoverBox } from "./upgradeHoverBox";
 import cursor from "../img/cursor.png";
+import hard_hat from "../img/Hard_Hat.png";
 import coal_lump from "../img/coal_lump_alt.png";
+import gas_symbol from "../img/Gas_Symbol.png";
+import sun from "../img/Sun.png";
 
 export function UpgradeSquare({
     watts, 
@@ -28,8 +31,14 @@ export function UpgradeSquare({
                     {
                         upgrade.generator === "Click" ? (
                             <img src={cursor} alt={upgrade.generator}/>
+                        ) : upgrade.generator === "Lineman" ? (
+                            <img src={hard_hat} alt={upgrade.generator}/>
                         ) : upgrade.generator === "Coal Plant" ? (
                             <img src={coal_lump} alt={upgrade.generator}/>
+                        ) : upgrade.generator === "Gas Plant" ? (
+                            <img src={gas_symbol} alt={upgrade.generator}/>
+                        ) : upgrade.generator === "Solar Farm" ? (
+                            <img src={sun} alt={upgrade.generator}/>
                         ) : (
                             <span>{upgrade.generator.charAt(0)}</span>
                         )
