bb07c9a91bccb1e8b9ff1c9e3541d1670ad9c2ab
Weldin Dunn <weldindunn20@gmail.com>
2022-07-08 11:30:44 -0400

    Fixed line spacing between generators

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -21,25 +21,23 @@ export function Generator({
     buyGenerator: (generator: string) => void;
 }): JSX.Element {
     return (
-        <>
-            <button className="generator" onClick={() => buyGenerator(name)} disabled={watts < Math.round(generatorCost)}>
-                <div className="generator-icon">
-                    <img src={icon} alt={name.charAt(0)}></img>
+        <button className="generator" onClick={() => buyGenerator(name)} disabled={watts < Math.round(generatorCost)}>
+            <div className="generator-icon">
+                <img src={icon} alt={name.charAt(0)}></img>
+            </div>
+            <div className="generator-content">
+                <div className="generator-title">
+                    {
+                        name.length > 13 ? (name.substring(0, 11) + "...") : (name)
+                    }
                 </div>
-                <div className="generator-content">
-                    <div className="generator-title">
-                        {
-                            name.length > 13 ? (name.substring(0, 11) + "...") : (name)
-                        }
-                    </div>
-                    <span className="generator-cost">
-                        {numberConvertor(Math.round(generatorCost))}
-                    </span>
-                    <div className="generator-count">
-                        {generators}
-                    </div>
+                <span className="generator-cost">
+                    {numberConvertor(Math.round(generatorCost))}
+                </span>
+                <div className="generator-count">
+                    {generators}
                 </div>
-            </button>
-        </>
+            </div>
+        </button>
     )
 }
