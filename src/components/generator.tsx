1fb39622498780d8a9e193ff6bfc95ccc0e242b7
Weldin Dunn <weldindunn20@gmail.com>
2022-07-11 11:44:29 -0400

    Added generator tooltips, still some problems with overlapping

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -1,43 +1,71 @@
 import React from "react";
+import { OverlayTrigger } from "react-bootstrap";
 import { numberConvertor } from "./utilities/numberConvertor";
 
 export function Generator({
     name,
     icon,
+    tooltip_icon,
     watts, 
     wattsPerSec,
     generators,
     generatorCost,
     generatorProduction,
+    description,
     buyGenerator
 } : {
     name: string,
     icon: string,
+    tooltip_icon: string,
     watts: number; 
     wattsPerSec: number;
     generators: number;
     generatorCost: number;
     generatorProduction: number;
+    description: string;
     buyGenerator: (generator: string) => void;
 }): JSX.Element {
     return (
-        <button className="generator" onClick={() => buyGenerator(name)} disabled={watts < Math.round(generatorCost)}>
-            <div className="generator-icon">
-                <img src={icon} alt={name.charAt(0)}></img>
-            </div>
-            <div className="generator-content">
-                <div className="generator-title">
-                    {
-                        name.length > 13 ? (name.substring(0, 11) + "...") : (name)
-                    }
+        <OverlayTrigger
+            placement={"left"}
+            overlay={
+                <div className="generator-tooltip">
+                    <div>
+                        <img src={tooltip_icon} alt={name}/>
+                        <span className="tooltip-name">{name}</span>
+                        <span className="tooltip-cost">{numberConvertor(Math.round(generatorCost))}</span>
+                    </div>
+                    <div>
+                        <span className="tooltip-description">{description}</span>
+                    </div>
+                    <div>
+                        <span className="tooltip-production">
+                            Each {name} produces {Math.round(generatorProduction/generators)} watts per second
+                            <br/>
+                            Your {name}s produce {generatorProduction} watts per second, {Math.round(generatorProduction/wattsPerSec * 100)}% of your total watts per second
+                        </span>
+                    </div>
                 </div>
-                <span className="generator-cost">
-                    {numberConvertor(Math.round(generatorCost))}
-                </span>
-                <div className="generator-count">
-                    {generators}
+            }
+        >
+            <button className="generator" onClick={() => buyGenerator(name)} disabled={watts < Math.round(generatorCost)}>
+                <div className="generator-icon">
+                    <img src={icon} alt={name.charAt(0)}></img>
                 </div>
-            </div>
-        </button>
+                <div className="generator-content">
+                    <div className="generator-title">
+                        {
+                            name.length > 13 ? (name.substring(0, 11) + "...") : (name)
+                        }
+                    </div>
+                    <span className="generator-cost">
+                        {numberConvertor(Math.round(generatorCost))}
+                    </span>
+                    <div className="generator-count">
+                        {generators}
+                    </div>
+                </div>
+            </button>
+        </OverlayTrigger>
     )
 }
--- src/components/viewGenerators.tsx
+++ src/components/viewGenerators.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Generator } from "./generator";
 import "./style/generators.css";
+
+//Icons
 import lineman from "../img/lineman_icon.png";
 import coal_plant from "../img/Coal_Plant_Icon.png";
 import gas_plant from "../img/Gas_Plant_Icon.png";
 import solar_farm from "../img/Solar_Farm_Icon.png";
+
+//Tooltip Icons
+import hard_hat from "../img/Hard_Hat.png";
+import coal_lump from "../img/coal_lump_alt.png";
+import gas_symbol from "../img/Gas_Symbol.png";
+import sun from "../img/Sun.png";
 
 export function ViewGenerators({
     watts,
@@ -85,91 +93,109 @@ export function ViewGenerators({
             <Generator
                 name="Lineman"
                 icon={lineman}
+                tooltip_icon={hard_hat}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={linemen}
                 generatorCost={linemenCost}
                 generatorProduction={linemenProduction}
+                description="Produces electricity by brute force"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Coal Plant"
                 icon={coal_plant}
+                tooltip_icon={coal_lump}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={coalPlants}
                 generatorCost={coalCost}
                 generatorProduction={coalProduction}
+                description="Burns coal to power steam engines"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Gas Plant"
                 icon={gas_plant}
+                tooltip_icon={gas_symbol}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={gasPlants}
                 generatorCost={gasCost}
                 generatorProduction={gasProduction}
+                description="Burns gas to power steam engines"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Solar Farm"
                 icon={solar_farm}
+                tooltip_icon={sun}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={solarFarms}
                 generatorCost={solarCost}
                 generatorProduction={solarProduction}
+                description="Harnaces the power of the sun to generate electricity"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Oil Well"
                 icon={gas_plant}
+                tooltip_icon={gas_symbol}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={oilWells}
                 generatorCost={oilCost}
                 generatorProduction={oilProduction}
+                description="Burns oil to power steam engines"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Wind Turbine"
                 icon={lineman}
+                tooltip_icon={hard_hat}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={windTurbines}
                 generatorCost={windCost}
                 generatorProduction={windProduction}
+                description="Uses wind to spin a turbine generator"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Biomass Gassifier"
                 icon={gas_plant}
+                tooltip_icon={gas_symbol}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={biomassGassifiers}
                 generatorCost={biomassCost}
                 generatorProduction={biomassProduction}
+                description="Burns biomass to power steam engines"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Hydro Plant"
                 icon={lineman}
+                tooltip_icon={hard_hat}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={hydroPlants}
                 generatorCost={hydroCost}
                 generatorProduction={hydroProduction}
+                description="Uses water power to spin turbine generators"
                 buyGenerator={buyGenerator}
             ></Generator>
             <Generator
                 name="Nuclear Plant"
                 icon={coal_plant}
+                tooltip_icon={hard_hat}
                 watts={watts}
                 wattsPerSec={wattsPerSec}
                 generators={nuclearPlants}
                 generatorCost={nuclearCost}
                 generatorProduction={nuclearProduction}
+                description="Uses the fission of uranium and plutonium to heat steam engines"
                 buyGenerator={buyGenerator}
             ></Generator>
         </div>
