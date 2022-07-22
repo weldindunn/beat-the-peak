54807cc308021e597bb0b09c7e0dc96a3ed918cc
Weldin Dunn <weldindunn20@gmail.com>
2022-07-22 09:13:41 -0400

    Adjusted heat wave and tornado curves

--- src/components/utilities/heatWaveCurve.tsx
+++ src/components/utilities/heatWaveCurve.tsx
@@ -5,5 +5,5 @@ import { integral } from "./integral"
 
 export function heatWaveCurve(time: number, deltaTime: number): number {
     // ((-1)*(x-210000)*(x-510,000))/3,000,000,000,000,000
-    return -1*integral(time - deltaTime, time, 210000, 510000, 3000000000000000);
+    return integral(time - deltaTime, time, 210000, 510000, 3000000000000000);
 }
