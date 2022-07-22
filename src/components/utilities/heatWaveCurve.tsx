42c5764f3e6f1603fc7c7c2bafd0e596cd80985e
Weldin Dunn <weldindunn20@gmail.com>
2022-07-22 09:30:01 -0400

    Adjusted heat wave curve, they were way too common!

--- src/components/utilities/heatWaveCurve.tsx
+++ src/components/utilities/heatWaveCurve.tsx
@@ -1,9 +1,9 @@
 //Takes in the time and returns a really really small number which represents the likelihood of a heat wave happening at that frame
-//With this expression, there is a 150% chance of a heat wave happening any given season (Apr-Sept)
+//With this expression, there is a 113% chance of a heat wave happening any given season (Apr-Sept)
 
 import { integral } from "./integral"
 
 export function heatWaveCurve(time: number, deltaTime: number): number {
-    // ((-1)*(x-210000)*(x-510,000))/3,000,000,000,000,000
-    return integral(time - deltaTime, time, 210000, 510000, 3000000000000000);
+    // ((-1)*(x-210000)*(x-510,000))/4,000,000,000,000,000
+    return -1*integral(time - deltaTime, time, 210000, 510000, 4000000000000000);
 }
