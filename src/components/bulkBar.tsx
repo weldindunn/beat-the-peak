50f9f4925d16684f71a0ac6939c210f17a20a748
Weldin Dunn <weldindunn20@gmail.com>
2022-07-29 15:21:48 -0400

    Made buy and sell buttons highlight when selected

--- src/components/bulkBar.tsx
+++ src/components/bulkBar.tsx
@@ -16,11 +16,11 @@ export function BulkBar({
 }): JSX.Element {
     return (
         <div className="bar">
-            <button onClick={() => setBuying(true)}>Buy</button>
-            <button onClick={() => setBuying(false)}>Sell</button>
-            <button onClick={() => {setTradeQuantity(1); setPriceModifier(1);}}>1</button>
-            <button onClick={() => {setTradeQuantity(10); setPriceModifier(20.303718238);}}>10</button>
-            <button onClick={() => {setTradeQuantity(100); setPriceModifier(7828749.671335256);}}>100</button>
+            <button className={isBuying ? 'glow-button' : 'default-button'} onClick={() => setBuying(true)}>Buy</button>
+            <button className={!isBuying ? 'glow-button' : 'default-button'} onClick={() => setBuying(false)}>Sell</button>
+            <button className={tradeQuantity === 1 ? 'glow-button' : 'default-button'} onClick={() => {setTradeQuantity(1); setPriceModifier(1);}}>1</button>
+            <button className={tradeQuantity === 10 ? 'glow-button' : 'default-button'} onClick={() => {setTradeQuantity(10); setPriceModifier(20.303718238);}}>10</button>
+            <button className={tradeQuantity === 100 ? 'glow-button' : 'default-button'} onClick={() => {setTradeQuantity(100); setPriceModifier(7828749.671335256);}}>100</button>
         </div>
     )
 }
