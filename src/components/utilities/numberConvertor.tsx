41d08543e92f30dbb4bdbb67640ca89a4d8e5dd6
Weldin Dunn <weldindunn20@gmail.com>
2022-08-01 08:25:15 -0400

    Fixed gittery numbers problem in suboptimal manner

--- src/components/utilities/numberConvertor.tsx
+++ src/components/utilities/numberConvertor.tsx
@@ -2,27 +2,35 @@ export function numberConvertor(num: number, isWatts: boolean): string {
     var convertedNum = "";
 
     if (num > 999999999999999) {
-        convertedNum = (num/1000000000000000).toFixed(3).toString().replace(/0+$/, '').replace(/\.$/, '');
+        convertedNum = (num/1000000000000000).toFixed(3).toString();//.replace(/0+$/, '').replace(/\.$/, '');
         if (isWatts) {
             convertedNum += "\n";
+        } else {
+            convertedNum = convertedNum.replace(/0+$/, '').replace(/\.$/, '');
         }
         convertedNum += " quadrillion";
     } else if (num > 999999999999) {
-        convertedNum = (num/1000000000000).toFixed(3).toString().replace(/0+$/, '').replace(/\.$/, '');
+        convertedNum = (num/1000000000000).toFixed(3).toString();//.replace(/0+$/, '').replace(/\.$/, '');
         if (isWatts) {
             convertedNum += "\n";
+        } else {
+            convertedNum = convertedNum.replace(/0+$/, '').replace(/\.$/, '');
         }
         convertedNum += " trillion";
     } else if (num > 999999999) {
-        convertedNum = (num/1000000000).toFixed(3).toString().replace(/0+$/, '').replace(/\.$/, '');
+        convertedNum = (num/1000000000).toFixed(3).toString();//.replace(/0+$/, '').replace(/\.$/, '');
         if (isWatts) {
             convertedNum += "\n";
+        } else {
+            convertedNum = convertedNum.replace(/0+$/, '').replace(/\.$/, '');
         }
         convertedNum += " billion";
     } else if (num > 999999) {
-        convertedNum = (num/1000000).toFixed(3).toString().replace(/0+$/, '').replace(/\.$/, '');
+        convertedNum = (num/1000000).toFixed(3).toString();//.replace(/0+$/, '').replace(/\.$/, '');
         if (isWatts) {
             convertedNum += "\n";
+        } else {
+            convertedNum = convertedNum.replace(/0+$/, '').replace(/\.$/, '');
         }
         convertedNum += " million";
     } else if (num > 999) {
